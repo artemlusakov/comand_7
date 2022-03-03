86419ddd4ba95af49cbe92c0a7139691a06c8641
CatalogReoflex <spraer11@mail.ru>
2022-03-03 13:26:41 +0300

    Main commit

---
+++ WebApplication1/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public int IdPosition { get; set; }
+        public string Fulename { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+}
---
+++ WebApplication1/Models/Position.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class Position
+    {
+        public int Id { get; set; }
+        public string Positions { get; set; } = string.Empty;
+
+    }
+}
